5001fd9835f546018c3a0f36a1730d9c3f4007bd
Jordan-Vicarius <jordan@vicarius.io>
2024-02-28 12:55:17 -0500

    Update DriverUpdatesLaunchCommand.ps1

--- DriverUpdates/prod/DriverUpdatesLaunchCommand.ps1
+++ DriverUpdates/prod/DriverUpdatesLaunchCommand.ps1
@@ -2,4 +2,4 @@
 Powershell.exe -executionpolicy bypass -command "rm -Force 'C:\Program Files\Vicarius\temp\vRxDrivers\driverUpdateLauncherLogs.txt' -ErrorAction SilentlyContinue; rm -force 'C:\Program Files\Vicarius\temp\vRxDrivers\DriverUpdates\DriverUpdates-MSUpdatelogs.txt' -ErrorAction SilentlyContinue; rm -force 'C:\Program Files\Vicarius\temp\vRxDrivers\PendingReboots\PendingRebootlogs.txt' -ErrorAction SilentlyContinue"
 Powershell.exe -executionpolicy bypass -command "Expand-Archive -path '.\vRxDriverupdates.zip' -DestinationPath '.\'"
 Powershell.exe -executionpolicy bypass -command ".\DriverUpdateLauncher.ps1"
-Powershell.exe -executionpolicy bypass -command "rm -Recurse -Force 'C:\Program Files\Vicarius\temp\vRxDrivers\DriverUpdates' -erroraction SilentlyContinue"
+Powershell.exe -executionpolicy bypass -command "rm -Recurse -Force 'C:\Program Files\Vicarius\temp\vRxDrivers\DriverUpdates' -ErrorAction SilentlyContinue"
